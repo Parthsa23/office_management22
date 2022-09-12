d7dfd1ac20fcd84392e85e35ce8bb44f48c4bdac
Parthsa23 <saraswatparth55@gmail.com>
2022-09-12 17:55:34 +0530

    changes in html files and merged roles and department models with employee models and worked on filter,remove,add and view all html files

--- office_emp_proj/emp_app/models.py
+++ office_emp_proj/emp_app/models.py
@@ -2,30 +2,18 @@ from django.db import models
 
 
 # Create your models here.
-class Department(models.Model):
-    name = models.CharField(max_length=90, blank=True)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Role(models.Model):
-    name = models.CharField(max_length=90, blank=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=90, blank=True)
     last_name = models.CharField(max_length=90)
-    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    dept =  models.CharField(max_length=90, blank=True)
     salary = models.IntegerField(default=0)
+    location = models.CharField(max_length=100, blank=True)
     bonus = models.IntegerField(default=0)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    role =models.CharField(max_length=90, blank=True)
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
 
     def __str__(self):
-        return "%s %s %s " %(self.first_name, self.last_name, self.role)
+        return "%s %s %s " % (self.first_name, self.last_name, self.role)
--- office_emp_proj/emp_app/views.py
+++ office_emp_proj/emp_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,HttpResponse
-from .models import Employee, Role, Department
+from django.shortcuts import render, HttpResponse
+from .models import Employee
 from datetime import datetime
-
+from django.db.models import Q
 
 
 # Create your views here.
@@ -25,23 +25,55 @@ def add_emp(request):
         last_name = request.POST['last_name']
         salary = int(request.POST['salary'])
         bonus = int(request.POST['bonus'])
-        dept= int(request.POST['dept'])
+        dept = request.POST['dept']
         phone = int(request.POST['phone'])
-        role = int(request.POST['dept'])
-        new_emp=Employee(first_name=first_name,last_name=last_name,salary=salary,bonus=bonus,phone=phone,dept_id=dept,role_id=role,hire_date=datetime.now())
+        role = request.POST['role']
+        hire_date = request.POST['hire_date']
+        location = request.POST['location']
+
+        new_emp = Employee(first_name=first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone,
+                           dept=dept, role=role, hire_date=datetime.now(),location=location)
         new_emp.save()
         return HttpResponse('Employee added Successfully')
-    elif request.method=='GET':
-        return render(request,'add_emp.html')
+
+    elif request.method == 'GET':
+        return render(request, 'add_emp.html')
     else:
         return HttpResponse("An Exception Occured! Employee")
 
 
+def remove_emp(request, emp_id=0):
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse("Employee Removed Successfully")
+        except:
+            return HttpResponse("Please Enter A Valid EMP ID ")
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
 
-
-def remove_emp(request):
-    return render(request, 'remove_emp.html')
+    }
+    return render(request, 'remove_emp.html', context)
 
 
 def filter_emp(request):
-    return render(request, 'filter_emp.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        dept = request.POST['dept']
+        # role = request.POST['role']
+        emps = Employee.objects.all()
+
+        if name:
+            emps = emps.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
+        if dept:
+            emps = emps.filter(Q(dept__icontains=dept))
+        if role:
+            emps = emps.filter(Q(role__icontains=role))
+        context = {
+            'emps': emps
+        }
+        return render(request, 'view_all_emp.html', context)
+    elif request.method == 'GET':
+        return render(request, 'filter_emp.html')
